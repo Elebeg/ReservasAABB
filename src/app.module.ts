c388bc3f51a10f3fcd37235851a2cdedff4a5b75
Lucas Schuler Foppa <lucasfoppa@hotmail.com>
2025-04-30 12:17:47 -0300

    Feature: deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { ReservationsModule } from './reservations/reservations.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
+      //url: process.env.DATABASE_URL,
       host: process.env.DATABASE_HOST,
       port: Number(process.env.DATABASE_PORT || 5432),
       username: process.env.DATABASE_USER,
